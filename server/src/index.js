f082375c1d9a6ab9282f4afd2e6f54071fdbc40d
nuhwilai <new@bonmek.com>
2021-10-23 21:51:14 +0700

    fixed cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.use("/api/books", require("./apis/book.api"));
 app.use("/api/authors", require("./apis/author.api"));
